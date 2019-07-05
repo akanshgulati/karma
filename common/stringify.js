c029020572bafd5b4b2489e50d027eabdc77ba81
Akansh Gulati <akanshgulati@gmail.com>
2019-07-06 00:01:07 +0530

    a

--- common/stringify.js
+++ common/stringify.js
@@ -2,7 +2,7 @@ var serialize = require('dom-serialize')
 var instanceOf = require('./util').instanceOf
 
 function isNode (obj) {
-  return (obj.tagName || obj.nodeName) && obj.nodeType
+  
 }
 
 function stringify (obj, depth) {
